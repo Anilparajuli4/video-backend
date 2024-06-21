cbf7e47155e4107ee10c6380db15d2267eb75758
Anil Parajuli <anilparajuli998@gmail.com>
2024-06-21 14:02:37 +0545

    create all route for user controller

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from '../controller/user.controller.js'
+import { changeCurrentPassword, getUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetail, updateUserAvatar, userCoverImage } from '../controller/user.controller.js'
 import { upload } from '../middleware/multer.js'
 import { verifyJWT } from '../middleware/auth.middleware.js'
 
@@ -21,5 +21,12 @@ router.post('/register', upload.fields([
 router.post('/login', loginUser)
 router.post('/logout', verifyJWT, logoutUser)
 router.post('/refreshToken',  refreshAccessToken)
+router.post('/changepassword', verifyJWT,  changeCurrentPassword)
+router.get('/currentuser', verifyJWT, getUser)
+router.patch('update-account', verifyJWT, updateAccountDetail)
+router.patch('/avatar', verifyJWT, upload.single('avatar'), updateUserAvatar)
+router.patch('/coverImage', verifyJWT, upload.single('coverImage'), userCoverImage)
+router.get('/channel/:username', verifyJWT, getUserChannelProfile)
+router.get('/history', verifyJWT, getWatchHistory)
 
 export default router 
